f641decf71c754189e4b531458aa7fe0111529ec
martyduniaud98 <marty.duniaud@ynov.com>
2022-04-20 16:33:52 +0200

    ClientHelpCommand

--- dist/App.f684dadd.js
+++ dist/App.f684dadd.js
@@ -21038,7 +21038,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63547" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59612" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
